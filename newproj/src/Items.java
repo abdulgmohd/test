7d103ce120b2c7edfdccbd16f4f09402b0c3b51c
abdul <abdulgmohd@gmail.com>
2019-02-09 18:38:14 -0800

    initial commit

---
+++ newproj/src/Items.java
@@ -0,0 +1,10 @@
+public class Items {
+    public static void main(String[] args){
+
+        String ftpuser = System.getProperty("FTP_USER");
+        String ftppassword= System.getProperty("FTP_USER");
+        System.out.println(ftpuser);
+        System.out.println(ftppassword);
+
+    }
+}
